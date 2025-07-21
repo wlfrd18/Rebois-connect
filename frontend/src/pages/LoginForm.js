55ab13c53fe85c7357e51be2a7f3a7f13e044614
wlfrd18 <wilfriedpanol@gmail.com>
2025-07-22 00:23:31 +0200

    add include credentials in the body request pf loginform

--- frontend/src/pages/LoginForm.js
+++ frontend/src/pages/LoginForm.js
@@ -22,6 +22,7 @@ export default function LoginForm() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
+        credentials: "include",
       });
 
       const data = await response.json();
