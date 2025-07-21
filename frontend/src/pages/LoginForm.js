ed844ba7e5a3b542c416c78c8bcacfa3cd68912c
wlfrd18 <wilfriedpanol@gmail.com>
2025-07-22 00:26:31 +0200

    remove include credentials in the body request pf loginform

--- frontend/src/pages/LoginForm.js
+++ frontend/src/pages/LoginForm.js
@@ -22,7 +22,6 @@ export default function LoginForm() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
-        credentials: "include",
       });
 
       const data = await response.json();
