1ff64e0b597156a4f152ceec31fba3a0f8da24f6
wlfrd18 <wilfriedpanol@gmail.com>
2026-02-08 04:34:29 +0100

    try to deploy the application

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,22 +1,23 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'une_clef_secrete_pour_dev')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://rebois_user:rebois_pass@localhost:5432/rebois_db')
+    # Clé secrète Flask
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # PostgreSQL (Render)
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'une_autre_clef_secrete')
-    # Configuration pour service SMS, par exemple Twilio
-    SMS_API_KEY = os.getenv('SMS_API_KEY')
-    SMS_API_SECRET = os.getenv('SMS_API_SECRET')
-    MAIL_SERVER = 'smtp-relay.brevo.com'
+
+    # JWT
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+    # Mail (Brevo)
+    MAIL_SERVER = "smtp-relay.brevo.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_DEFAULT_SENDER = 'reboisconnect@gmail.com'
-    MAIL_USERNAME = '8efba4001@smtp-brevo.com'
-    MAIL_PASSWORD = os.getenv('BREVO_SMTP_KEY')
-    SECRET_KEY = 'Master Password'
-    DEBUG = True
+    MAIL_USERNAME = "apikey"
+    MAIL_PASSWORD = os.getenv("BREVO_SMTP_KEY")
+    MAIL_DEFAULT_SENDER = "reboisconnect@gmail.com"
+
+    # Mode debug
+    DEBUG = False
